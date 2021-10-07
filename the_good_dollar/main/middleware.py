e25cfe88c9e24bea7058e570db64ff4f934ebfbb
fouadsan <f.benayad95@gmail.com>
2021-10-07 19:12:25 +0100

    Improved admin panel

--- the_good_dollar/main/middleware.py
+++ the_good_dollar/main/middleware.py
@@ -8,26 +8,9 @@ class ViewersMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
-        # conn = sqlite3.connect('db.sqlite3')
-        # c = conn.cursor()
 
     def viewer_count(self):
-
-        # # get the count of tables with the name
-        # self.c.execute(
-        #     ''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='students' ''')
-
-        # # if the count is 1, then table exists
-        # if self.c.fetchone()[0] == 1:
-
-        #     print('Table exists.'),
-
         Viewer.objects.update(count=F('count') + 1)
-
-        # commit the changes to db
-        # self.conn.commit()
-        # close the connection
-        # self.conn.close()
 
     def __call__(self, request):
         if request.path == "/":
