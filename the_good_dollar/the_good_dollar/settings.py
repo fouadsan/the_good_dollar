b7926c9b865689d51b7a9c10d63261592b95652b
Fouad B <74966625+fouadsan@users.noreply.github.com>
2022-01-03 18:16:19 +0100

    Update settings.py

--- the_good_dollar/the_good_dollar/settings.py
+++ the_good_dollar/the_good_dollar/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$pq(y#8qa=ovi)2fc(q-o4^)3lgtoh63gagaa4azm0^hs0%ec$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
